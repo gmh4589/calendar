c01d6389f5f25fa820e32a80b1c9ec32cc05a627
Alex <41452060+gmh4589@users.noreply.github.com>
2024-12-04 12:04:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,104 +1,212 @@
-
+import calendar
 import datetime
+import os
+from random import randint
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.codeinput import CodeInput
+from kivy.core.window import Window
 
 Builder.load_file('calendar.kv')
 
-flag = int(datetime.datetime.now().strftime("%m"))
+def rgb(color):
 
-class MonthScreen(Screen):
+	return (
+		color[0] / 255,
+		color[1] / 255,
+		color[2] / 255,
+		color[3] / 255,
+	)
 
-	# weekDays = ['ПОНЕДЕЛЬНИК', 'ВТОРНИК', 'СРЕДА', 'ЧЕТВЕРГ', 'ПЯТНИЦА', 'СУББОТА', 'ВОСКРЕСЕНЬЕ']
-	weekDays = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС']
-	month = {1:"ЯНВАРЬ", 2:"ФЕВРАЛЬ", 3:"МАРТ", 4:"АПРЕЛЬ", 5:"МАЙ", 6:"ИЮНЬ", 7:"ИЮЛЬ", 8:"АВГУСТ", 9:"СЕНТЯБРЬ", 10:"ОКТЯБРЬ", 11:"НОЯБРЬ", 12:"ДЕКАБРЬ"}
-	month2 = int(datetime.datetime.now().strftime("%m"))
-	year = int(datetime.datetime.now().strftime("%Y"))
+class CustomButton(Button):
 
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
+		self.color = (.2, .2, .2, 1)
+		self.background_color = (.76, .81, .82, 1)
+		self.background_normal = ''
+		self.background_down = ''
 
-		for day in self.weekDays:
-			self.ids.week.add_widget(Label(text = day, font_size = 30))
 
-		self.createMonth(self.month2, self.year)
+class MonthScreen(Screen):
 
-	def monthDay(self, year, mon):
+	week_days = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС']
+	months = {
+		1:  ["ЯНВАРЬ", rgb((128, 200, 200, 200))],
+	    2:  ["ФЕВРАЛЬ", rgb((36, 200, 204, 200))],
+	    3:  ["МАРТ", rgb((220, 220, 60, 200))],
+	    4:  ["АПРЕЛЬ", rgb((171, 230, 26, 200))],
+	    5:  ["МАЙ", rgb((112, 200, 32, 200))],
+	    6:  ["ИЮНЬ", rgb((252, 30, 12, 200))],
+	    7:  ["ИЮЛЬ", rgb((230, 12, 50, 200))],
+	    8:  ["АВГУСТ", rgb((255, 81, 81, 200))],
+	    9:  ["СЕНТЯБРЬ", rgb((255, 201, 47, 200))],
+	    10: ["ОКТЯБРЬ", rgb((255, 133, 50, 200))],
+	    11: ["НОЯБРЬ", rgb((255, 99, 64, 200))],
+	    12: ["ДЕКАБРЬ", rgb((126, 182, 254, 200))]
+			  }
 
-		match mon:
+	w, h = Window.width, Window.height
+	font_size = int(w / 20)
+	month_now = int(datetime.datetime.now().strftime("%m"))
+	year_now = int(datetime.datetime.now().strftime("%Y"))
+	root_path = os.path.dirname(os.path.abspath(__file__))
+	background_source = StringProperty(f"{root_path}/months/{month_now}/{randint(1, 5)}.jpg")
 
-			case 1 | 3 | 5 | 7 | 8 | 10 | 12:
-				return 31
-			case 4 | 6 | 9 | 11:
-				return 30
-			case 2:
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.move = []
+		self.plus_month = None
+		self.minus_month = None
+		self.old = None
 
-				if year % 400 == 0:
-					return 29
+		self.lbl1 = Label(text='МЕСЯЦ:')
+		self.lbl2 = Label(text='ГОД:')
+		self.tf1 = CodeInput(font_size=40)
+		self.tf2 = CodeInput(font_size=40)
+		self.b_ok = CustomButton(text='ПЕРЕЙТИ')
+		self.b_cancel = CustomButton(text='ОТМЕНА')
+		self.lbl3 = Label()
+		self.lbl4 = Label()
 
-				if year % 100 == 0:
-					return 28
+		for day in self.week_days:
+			self.ids.week.add_widget(Label(text = day, font_size = self.font_size))
 
-				if year % 4 == 0:
-					return 29
+		self.create_month(self.month_now, self.year_now)
+		self.ids.nowBTN.text = f"СЕЙЧАС: {self.months[self.month_now][0]} {self.year_now}"
+		self.ids.nowBTN.on_press = lambda: self.create_month(self.month_now, self.year_now)
 
-				return 28
+	def now_date_update(self, dt):
+		self.ids.nowBTN.text = f"СЕЙЧАС: {self.months[int(datetime.datetime.now().strftime('%m'))][0]} {int(datetime.datetime.now().strftime('%Y'))}"
 
-	def createMonth(self, mon, year):
+	def on_touch_down(self, touch):
+		self.move = [touch.x]
+		return super().on_touch_down(touch)
+
+	def on_touch_move(self, touch):
+		self.move.append(touch.x)
+		return super().on_touch_move(touch)
+
+	def on_touch_up(self, touch):
+
+		if len(self.move) >= 2:
+
+			if self.move[0] > self.move[-1]:  # Свайп влево
+				self.plus_month()
+			elif self.move[0] < self.move[-1]:  # Свайп вправо
+				self.minus_month()
+
+		self.move.clear()
+		return super().on_touch_up(touch)
+
+	def create_month(self, mon, year):
+		Window.clearcolor = self.months[mon][1]
+
+		if self.ids.dateScreen.cols != 7:
+			return
+
 		prw = mon - 1 if mon - 1 != 0 else 12
 		nxt = mon + 1 if mon + 1 != 13 else 1
-		self.setMonth(mon, year)
+		self.ids.month.clear_widgets()
+		self.background_source = f"{self.root_path}/months/{mon}/{randint(1, 5)}.jpg"
+		self.ids.month.add_widget(Label(text=self.months[mon][0] + ' ' + str(year), font_size=self.font_size))
+		self.ids.backBTN.text = self.months[prw][0]
+		self.ids.forwBTN.text = self.months[nxt][0]
+		self.ids.backBTN.background_color = self.months[prw][1]
+		self.ids.forwBTN.background_color = self.months[nxt][1]
+		self.plus_month = lambda: self.change_month(nxt, year if mon != 12 else year + 1)
+		self.minus_month = lambda: self.change_month(prw, year if mon != 1 else year - 1)
+		self.ids.backBTN.on_press = self.minus_month
+		self.ids.forwBTN.on_press = self.plus_month
+		self.ids.dateScreen.clear_widgets()
+		days = calendar.monthrange(year, mon)[-1]
 
-		self.ids.backBTN.text = self.month[prw]
-		self.ids.forwBTN.text = self.month[nxt]
-		self.ids.backBTN.on_press = lambda: self.chngMonth(prw, year, -1)
-		self.ids.forwBTN.on_press = lambda: self.chngMonth(nxt, year, 1)
-
-		self.enptyWidget(datetime.date(year, mon, 1).isoweekday() - 1)
-		self.setDays(self.monthDay(year, mon), mon, year)
-
-	def setDays(self, days, mon, year):
+		for i in range(datetime.date(year, mon, 1).isoweekday() - 1):
+			self.ids.dateScreen.add_widget(Label(text = ''))
 
 		for i in range(days):
 
-			if datetime.datetime.now().strftime('%Y-%m-%d') == str(datetime.date(year, mon, i + 1)): 
-				color = (.1, .8, .2, 1)
-			elif datetime.date(year, mon, i + 1).isoweekday() == 6 or datetime.date(year, mon, i + 1).isoweekday() == 7: 
-				color = (.85, .1, .1, 1)
+			if datetime.datetime.now().strftime('%Y-%m-%d') == str(datetime.date(year, mon, i + 1)):
+				color = (.1, .8, .2, .75)
+			elif datetime.date(year, mon, i + 1).isoweekday() == 6 or datetime.date(year, mon, i + 1).isoweekday() == 7:
+				color = (.85, .1, .1, .75)
 			else:
-				color = (.45, .45, .6, 1)
-			
+				color = (.95, .95, .95, .75)
+
 			self.ids.dateScreen.add_widget(
-						Button(text = str(i + 1), 
-						font_size = 30, background_normal = '',
-						background_down = '', 
+						Button(text = str(i + 1),
+					    color=(.2, .2, .2, 1),
+						font_size = self.font_size,
+					    background_normal = '',
+						background_down = '',
 						background_color = color))
 
-	def setMonth(self, mon, year):
-		self.ids.month.clear_widgets()
-		self.ids.month.add_widget(Label(text = self.month[mon] + ' ' + str(year), font_size = 30))
+	def change_month(self, mon, year):
 
-	def enptyWidget(self, num):
-		for i in range(num): self.ids.dateScreen.add_widget(Label(text = ''))
+		if self.ids.dateScreen.cols == 7 and 0 < year < 10000:
+			self.ids.dateScreen.clear_widgets()
+			self.create_month(mon, year)
 
-	def chngMonth(self, mon, year, f):
-		global flag
-		flag += f
-		plus = flag // 13
+	def jump_to_date(self):
 
-		if flag == 0:
-			flag = 12
-			year -= 1
-		else: year += plus
+		if self.old is None:
+			self.old = list(self.ids.dateScreen.children)
 
 		self.ids.dateScreen.clear_widgets()
-		self.createMonth(mon, year)
+		self.ids.dateScreen.rows = 8
+		self.ids.dateScreen.cols = 2
+		self.tf1.text = ''
+		self.tf2.text = ''
+		self.lbl3.text = ''
+		self.lbl4.text = ''
+		self.b_cancel.on_press = lambda *args: self.cancel_jump(self.old)
+		self.b_ok.on_press = lambda *args: self.apply_jump(self.tf1.text, self.tf2.text)
 
-		if plus != 0: flag = 1
+		self.ids.dateScreen.add_widget(self.lbl1)
+		self.ids.dateScreen.add_widget(self.lbl2)
+		self.ids.dateScreen.add_widget(self.tf1)
+		self.ids.dateScreen.add_widget(self.tf2)
+		self.ids.dateScreen.add_widget(self.b_ok)
+		self.ids.dateScreen.add_widget(self.b_cancel)
+		self.ids.dateScreen.add_widget(self.lbl3)
+		self.ids.dateScreen.add_widget(self.lbl4)
+
+		for _ in range(8):
+			self.ids.dateScreen.add_widget(Label())
+
+	def apply_jump(self, m, y):
+
+		try:
+			m1 = int(m)
+			y1 = int(y)
+
+			if 0 < m1 < 13 and 0 < y1 < 10000:
+				self.ids.dateScreen.rows = 6
+				self.ids.dateScreen.cols = 7
+				self.old = None
+				self.change_month(m1, y1)
+			else:
+				self.lbl3.text = 'ВВЕДИТЕ КОРРЕКТУЮ ДАТУ!'
+				self.lbl4.text = ''
+
+		except (TypeError, ValueError):
+			self.lbl3.text = 'ВВЕДИТЕ КОРРЕКТУЮ ДАТУ!'
+			self.lbl4.text = ''
+
+	def cancel_jump(self, old):
+		self.ids.dateScreen.clear_widgets()
+		self.ids.dateScreen.rows = 6
+		self.ids.dateScreen.cols = 7
+		self.old = None
+
+		for widget in reversed(old):
+			self.ids.dateScreen.add_widget(widget)
+
 
 class CalendarApp(App):
 
@@ -109,5 +217,6 @@ class CalendarApp(App):
 		sm = ScreenManager()
 		sm.add_widget(MonthScreen(name='NowYear'))
 		return sm
+
 
 CalendarApp().run()
